f83dc41147d10db9e1ea8c537ee74b725acbab47
Daniel Flores D <daniel.flor3s.d@gmail.com>
2023-02-15 11:34:53 -0700

    Starter code an initial ApolloServer impl

---
+++ server/seeders/seed.js
@@ -0,0 +1,30 @@
+const db = require("../config/connection");
+const { User, Book } = require("../models");
+const userSeeds = require("./userSeeds.json");
+const bookSeeds = require("./bookSeeds.json");
+const userController = require("../controllers/user-controller");
+
+db.once("open", async () => {
+  try {
+
+    await User.deleteMany({});
+
+    userSeeds.forEach((userSeed) => {
+      const randomBooks = bookSeeds.sort(() => Math.random() - 0.5).slice(0, 2);
+      userSeed.savedBooks = randomBooks;
+
+    });
+
+    await User.create(userSeeds);
+
+    let users = await User.find({});
+    console.log(users);
+  
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log("all done!");
+  process.exit(0);
+});
