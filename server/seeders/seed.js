6811ce672c91fffbd34e6ba7ba1aad1f98a983e9
Daniel Flores D <daniel.flor3s.d@gmail.com>
2023-02-18 10:49:51 -0700

    make it better

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -3,12 +3,15 @@ const { User, Book } = require("../models");
 const userSeeds = require("./userSeeds.json");
 const bookSeeds = require("./bookSeeds.json");
 const userController = require("../controllers/user-controller");
+const { compareSync } = require("bcrypt");
 
 db.once("open", async () => {
   try {
 
+    console.log("deleting books...")
     await User.deleteMany({});
 
+    // Set random books for each user
     userSeeds.forEach((userSeed) => {
       const randomBooks = bookSeeds.sort(() => Math.random() - 0.5).slice(0, 2);
       userSeed.savedBooks = randomBooks;
